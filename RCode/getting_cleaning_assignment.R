e2503deec814ded2909f9006182b05c9d7d2936a
ahmad <tanvir073@yahoo.com>
2016-12-14 00:12:08 +0600

    update r script

--- RCode/getting_cleaning_assignment.R
+++ RCode/getting_cleaning_assignment.R
@@ -19,24 +19,65 @@
 setwd("C:/tanvir/Tutorial/")
 ##download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","data/Dataset.zip")
 
-## load train and test data set
+## test area
 
-traindatax<-read.table("MyData/UCI HAR Dataset/train/X_train.txt")
-traindatax$Axis='x'
-traindatay<-read.table("MyData/UCI HAR Dataset/train/y_train.txt")
-traindatay$Axis='y'
-traindata<-rbind(traindatax,traindatay)
+typeof(Labels)
+str(Labels)
+head(Labels)
+
+
+## load train and test data set and merge two data set
+
+traindata<-read.table("MyData/UCI HAR Dataset/train/X_train.txt")
+Lavels<-read.table("MyData/UCI HAR Dataset/train/y_train.txt",col.names = 'Lavels')
 traindata$DataType='Train'
+traindata$Lavels=Lavels$Lavels
 head(traindata,2)
 testdata<-read.table("MyData/UCI HAR Dataset/test/X_test.txt")
 testdata$DataType='Test'
+Lavels<-read.table("MyData/UCI HAR Dataset/test/y_test.txt",col.names = 'Lavels')
+testdata$Lavels=Lavels$Lavels
 head(testdata,2)
 
 mergeddata<-rbind(traindata,testdata)
 
 head(mergeddata,2)
 
-dim(traindatay)
+## Extracts only the measurements on the mean and standard deviation for each measurement.
+
+library(dplyr)
+header<-read.table("MyData/UCI HAR Dataset/features.txt",sep = " ",as.is = c(1,2))
+
+head(header)
+
+header=gsub("__","_",gsub("__","_",gsub("[-,()]","_",header$V2)))
+
+header[grep("mean_|std_",header)]
+grep("mean_|std_",header)
+
+mergeddata1=mergeddata
+
+x='tBodyAcc_mean_X'
+header[x]
+paste('V',1,sep='')
+y=paste('rename(mergeddata1,',x,paste('=V',1,')',sep=''),sep = '')
+y
+eval(y)
+eval(parse(text=y),envir = parent.frame(2))
+
+head(mergeddata1[1])
+
+head(rename(mergeddata1,tBodyAcc_mean_X=V1))
+
+head(select(mergeddata1,x=matches("V1")))
+
+mergeddata1[[paste('V',x,sep='')]]
+
+mergeddata1[,colnames(paste('V',x,sep=''))]
+
+mEx<-expression(y)
+eval(mEx)
+
 
 
 
