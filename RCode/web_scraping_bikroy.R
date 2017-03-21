a819a04db3a1bcdb636a1638ed8cafb7d4885e3a
ahmad <tanvir073@yahoo.com>
2017-03-21 23:33:53 +0600

    two script files added for web scraping

---
+++ RCode/web_scraping_bikroy.R
@@ -0,0 +1,41 @@
+## library(rvest)
+
+setwd("C:/tanvir/Tutorial/")
+
+AddData=NULL
+for(i in 1:5){
+  
+  
+  link_page_url=paste("https://bikroy.com/en/ads?page=",i,sep = "")
+  link_page_html=read_html(link_page_url)
+  for (j in 1:40) {
+    
+    add_url=link_page_html %>% html_node(paste(".ui-item:nth-child(",j,") a",sep=""))  %>% html_attr(name = 'href') ### mobile number
+    
+    if(is.na(add_url)) break()
+    
+    add_url=paste("https://bikroy.com",add_url,sep = "")
+    
+    add_html=read_html(add_url)
+    
+    Name=add_html %>% html_node(".poster")  %>% html_text ### name
+    MobileNumber=add_html %>% html_node(".clearfix")  %>% html_text ### mobile number
+    Product=add_html %>% html_node("h1")  %>% html_text ### product
+    ItemCategory1=add_html %>% html_node(".ui-crumb:nth-child(2) a")  %>% html_text ## ItemCategory1
+    ItemCategory2=add_html %>% html_node(".ui-crumb:nth-child(3) a")  %>% html_text ## ItemCategory2
+    Adress=add_html %>% html_node(".ui-crumb:nth-child(3) span")  %>% html_text ## Adress
+    Price=add_html %>% html_node(".ui-price-tag")  %>% html_text ## Price
+    AddDate=add_html %>% html_node(".date")  %>% html_text ## AddDate
+    
+    NewData=data.frame(Name=Name,MobileNumber=MobileNumber,Adress=Adress,AddDate=AddDate,ItemCategory1=ItemCategory1,ItemCategory2=ItemCategory2,Product=Product,AddUrl=add_url,AddPageNum=i,AddNum=j)
+    AddData=rbind(NewData,AddData)
+    
+  }
+  
+}
+
+AddData$Product[AddData$AddUrl=="https://bikroy.com/en/ad/ddim-theke-baaccaa-phottaanr-sbyynkriyy-yntr-for-sale-chittagong-division"]
+
+AddData$AddUrl=="https://bikroy.com/en/ad/ddim-theke-baaccaa-phottaanr-sbyynkriyy-yntr-for-sale-chittagong-division"
+
+write.table(x=AddData,file='bikroyweb.csv',sep=',',row.names = F,append = T)
--- RCode/web_scraping_mosq.R
+++ RCode/web_scraping_mosq.R
@@ -4,15 +4,93 @@
 
 setwd("C:/tanvir/Tutorial/")
 
-vignette("selectorgadget") 
+## vignette("selectorgadget") 
 
-mosq_html<-read_html("http://www.mosquedirectory.co.uk/mosques/england/london/harrow/wealdstone/Harrow-Central-Mosque-and-Masood-Islamic-Centre-Middlesex-Harrow/393")
+##### function to get mosque data table ### started
 
-data=mosq_html %>% html_node("#mosque_info_contents > table")  %>% html_table(fill=T)
 
-table_data=data.frame(MosqID=1,AttributeName=data$X1[!is.na(data$X1)],AttributeValue=data$X2[!is.na(data$X1)])
+getMosqData=function(mosq_url){
+  
+  mosq_url=gsub( "[\n\r\t]","",mosq_url)
+  mosq_html=NULL
+  mosq_html=tryCatch(
+    read_html(mosq_url)
+    ,error=function(e){
+      write.table(mosq_url,file='ErrorLink.csv',row.names = F,append = T,col.names = F)
+    }
+  )
+  
+  if(length(mosq_html)==0) table_data=NULL
+  else {
+    
+    MosqName=mosq_html %>% html_node("td h1")  %>% html_text
+    
+    if(!is.na(MosqName)){
+      data=mosq_html %>% html_node("#mosque_info_contents > table")  %>% html_table(fill=T)
+      table_data=data.frame(MosqName,AttributeName=data$X1[!is.na(data$X1)],AttributeValue=data$X2[!is.na(data$X1)])
+      
+    }
+    else {
+      table_data=NULL
+      write.table(mosq_url,file='ErrorLink.csv',row.names = F,append = T,col.names = F)
+    }
+    
+  }
+  
+  table_data
+  
+}
 
-write.table(x=table_data,file='web.csv',sep=',',row.names = F)
+#### url='http://www.mosquedirectory.co.uk/mosques/england/london/harrow/wealdstone/Harrow-Central-Mosque-and-Masood-Islamic-Centre-Middlesex-Harrow/393'
+
+#### getMosqData(url)
+
+##### function to get mosque data table ### ended
+
+
+###### write mosque information to csv 
+
+for(alpa in LETTERS)
+{
+
+  ## if(alpa<="W") next() #### For fix letter start
+  
+  mosq_link_page_url=paste("http://www.mosquedirectory.co.uk/browse-mosques/alphabet/letter/",alpa,"/",1,sep = "")
+  
+  mosq_link_page=read_html(mosq_link_page_url)
+  
+  mosq_link=mosq_link_page %>% html_node(paste("#search_data_results > ul > li:nth-child(",1,") > a"))  %>% html_attr(name = 'href')
+  
+  if(is.na(mosq_link)) next()
+  
+  for(i in 1:500)
+  {
+    mosq_link_page=read_html(paste("http://www.mosquedirectory.co.uk/browse-mosques/alphabet/letter/",alpa,"/",i,sep = ""))
+    ##print(paste("http://www.mosquedirectory.co.uk/browse-mosques/alphabet/letter/",alpa,"/",i,sep = ""))
+    mosq_link=mosq_link_page %>% html_node(paste("#search_data_results > ul > li:nth-child(",1,") > a"))  %>% html_attr(name = 'href')
+    
+    if(is.na(mosq_link)) break()
+    
+    mosq_data=NULL
+    
+    for(j in 1:500)
+    {
+      mosq_link=mosq_link_page %>% html_node(paste("#search_data_results > ul > li:nth-child(",j,") > a"))  %>% html_attr(name = 'href')
+      
+      if(is.na(mosq_link)) break()
+      mosq_link=sub("../../../../..","http://www.mosquedirectory.co.uk",mosq_link)
+      mosq_data=rbind(mosq_data,getMosqData(mosq_link))
+      
+    }
+    if(length(mosq_data)!=0) write.table(x=mosq_data,file='web.csv',sep=',',row.names = F,append = T)
+    
+  }
+  
+}
+
+
+
+
 
 
 
